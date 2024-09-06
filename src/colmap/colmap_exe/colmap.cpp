f80862d378fb02bf1f63eafa5937c331129f8b5e
fenghuayumo <fhymyang@163.com>
2024-09-06 17:37:53 +0800

    [M] fix linux compile

--- src/colmap/colmap_exe/colmap.cpp
+++ src/colmap/colmap_exe/colmap.cpp
@@ -1,5 +1,6 @@
 #include <colmap/dll/colmap_lib.h>
-
+#include <thread>
+#include <iostream>
 int main() 
 { 
 	ColmapSparseReconstruct sparse_reconstruct;
@@ -7,7 +8,18 @@ int main()
 	sparse_reconstruct.option.workspace_path = "G:/tandt_db/3dgs_test_video/sofa2/";
     sparse_reconstruct.option.use_glomap = true;
     sparse_reconstruct.option.output_sparse_points = true;
-    sparse_reconstruct.run();
+	std::thread t([&](){
+    	sparse_reconstruct.run();
+	});
+	t.detach();
+
+	while(1){
+		//std::cout << "progress: " << sparse_reconstruct.GetProgressOnCurrentPhase() << std::flush;
+
+		// std::cout << "";
+                const auto& p = sparse_reconstruct.getImageTracks(0);
+		std::cout << "size:" << p.size() << std::flush;
+	}
 
 	return 0;
 }
